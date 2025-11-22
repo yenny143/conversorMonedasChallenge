bf188cbe842c8b61be23b9ff578ce581a974d326
yenny143 <yennylisbeth143@gmail.com>
2025-11-22 01:45:32 -0300

    quito los errores

--- src/ConsultarMoneda.java
+++ src/ConsultarMoneda.java
@@ -1,6 +1,4 @@
 import com.google.gson.Gson;
-
-import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
--- src/Principal.java
+++ src/Principal.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 public class Principal {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException{
 
         ConsultarMoneda consultarMoneda = new ConsultarMoneda();
         Moneda moneda =consultarMoneda.convertirMoneda("USD","ARS");
